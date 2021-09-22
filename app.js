1581ed5f4a44063b3de2e8bf8e57eb7750c1e096
Roonie <roonierue@gmail.com>
2021-09-22 16:26:57 +0200

    complete

---
+++ app.js
@@ -0,0 +1,41 @@
+const randDrink = document.getElementById('random-drink'),
+      showDrink =document.getElementById('drinks'),
+      displayHeading = document.getElementById('display-heading');
+      
+
+
+function getCocktail(){
+
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+    .then(res => res.json())
+    .then(data =>{
+        console.log(data);
+
+            displayHeading.innerHTML = data.drinks.map(drink =>
+
+                `
+                <div class="card text-center card-body mt-3 mx-auto" style="width: 22rem;">
+                    <div class="row">
+                        <div class="col-md">
+                            <h2 class="mt-3">Get inspired by : <stong>${drink.strDrink}<stong> </h2>
+                        <div>
+                            <img src="${drink.strDrinkThumb}" alt="${drink.strDrink}"class="img-fluid mb-5 rounded"/>
+                        </div>
+                    </div>
+                </div>
+                `)
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+             
+    }
+
+
+randDrink.addEventListener('click', getCocktail);
+
+
+
+
+
+
